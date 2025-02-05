cd29ccfcd27952dc644cbee54082192c60914c73
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2025-02-05 11:45:33 +0600

    Update 03.js
    remove multi-line comments

--- 01/03.js
+++ 01/03.js
@@ -2,7 +2,7 @@
 let a = 4;
 let b = 2;
 let result;
-/*
+
 // add
 result = a + b;
 console.log( result );
@@ -26,15 +26,15 @@ console.log( result );
 // power
 result = a ** b;
 console.log( result );
- */
 
-/*
+
+
 //concatenation
 a = "Mazidul";
 b = "Islam";
 result = a + " " + b;
 console.log(result);
-*/
+
 
 
 //? ECMA rule -> practice only not for production
