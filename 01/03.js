fb3765c433271cd7ffd8a38d71d7de5626fa7274
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 11:43:43 +0600

    ECMA rule -> practice only not for production

--- 01/03.js
+++ 01/03.js
@@ -2,7 +2,7 @@
 let a = 4;
 let b = 2;
 let result;
-
+/*
 // add
 result = a + b;
 console.log( result );
@@ -26,5 +26,20 @@ console.log( result );
 // power
 result = a ** b;
 console.log( result );
+ */
 
+/*
+//concatenation
+a = "Mazidul";
+b = "Islam";
+result = a + " " + b;
+console.log(result);
+*/
+
+
+//? ECMA rule -> practice only not for production
+console.log("1" + 2); // 12 
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2);// 122
+console.log(1 + 2 + "2");// 32
 
