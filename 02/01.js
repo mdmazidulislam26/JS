902a7c22a6483f416235b38b3c0c12482eb0eeb3
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 16:12:18 +0600

    array

---
+++ 02/01.js
@@ -0,0 +1,12 @@
+// array
+
+const arr1 = [1,2,3];
+const arr2 = new Array("hello","Hi");
+const arr3 = new Array();
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+// array method
+
