cc375ffc161afe2bbab0936134f18a44a8c92b1d
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 11:22:27 +0600

    math operation

---
+++ 01/03.js
@@ -0,0 +1,30 @@
+/** Operation */
+let a = 4;
+let b = 2;
+let result;
+
+// add
+result = a + b;
+console.log( result );
+
+// subtract
+result = a - b;
+console.log( result );
+
+// multiply
+result = a * b;
+console.log( result );
+
+// division
+result = a / b;
+console.log( result );
+
+// Mod
+result = a % b;
+console.log( result );
+
+// power
+result = a ** b;
+console.log( result );
+
+
