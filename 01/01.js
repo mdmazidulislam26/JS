478e52aaacba2050fa0b3cb747ca756b4599290e
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 09:52:16 +0600

    chai aur code

---
+++ 01/01.js
@@ -0,0 +1,7 @@
+const name = "Md Mazidul Islam"; // const - unmodifiable
+let regId = 12009326; // let - modifiable - in scope not globally
+var department = "CSE"; // var - modifiable - globally - jaw it not recommended and not used 
+university = "Lovely Professional University";
+
+// console.log(name);
+console.table([name,regId,department,university]);
