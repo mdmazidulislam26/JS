726e644e2a5bd7b7ba6c0567eb22f6956e704b24
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 10:27:31 +0600

    data types

---
+++ 01/02.js
@@ -0,0 +1,21 @@
+let a = 10; 
+let b = 'A';
+let c = true;
+let d = null;
+let e;
+
+/**
+    a -> number 
+    b -> string
+    c -> boolean
+    d -> null and object 
+    e -> undefined
+    etc...
+*/
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
