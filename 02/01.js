8bd93cea4275f0576d870cf6b0a280f3b663d3a0
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2025-02-05 17:01:52 +0600

    Update 01.js
    remove comment

--- 02/01.js
+++ 02/01.js
@@ -4,9 +4,9 @@ const arr1 = [1,2,3];
 const arr2 = new Array("hello","Hi");
 const arr3 = new Array();
 
-// console.log(arr1);
-// console.log(arr2);
-// console.log(arr3);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
 
 // array method
 
@@ -16,25 +16,24 @@ arr3.push(3);
 arr3.push(4);
 arr3.push(5);
 arr3.push("Hello");
-// console.log(arr3);
-// arr3.pop()
-// console.log(arr3);
-// arr3.unshift();
-// console.log(arr3);
-// arr3.shift();
-// console.log(arr3);
+console.log(arr3);
+arr3.pop()
+console.log(arr3);
+arr3.unshift();
+console.log(arr3);
+arr3.shift();
+console.log(arr3);
 
-// console.log(arr3.includes(0));
-// console.log(arr3.indexOf(2));
+console.log(arr3.includes(0));
+console.log(arr3.indexOf(2));
 
-// const strArr = arr3.join();
+const strArr = arr3.join();
 
-// console.log(strArr);
-// console.log(typeof strArr);
+console.log(strArr);
+console.log(typeof strArr);
 
-
-// slice => Not modify the array
-// splice => modify the array
+slice => Not modify the array
+splice => modify the array
 
 console.log("A ", arr3);
 console.log(arr3.slice(1,4));
