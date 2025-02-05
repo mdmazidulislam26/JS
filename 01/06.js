20d0344d0f2ddf3aaf18d685d4e220f9df47f5df
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-05 15:48:30 +0600

    Dat and Time

---
+++ 01/06.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+
+// console.log(typeof(myDate));
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let myDOB = new Date("2001-04-16");
+console.log(myDOB.toLocaleString());
