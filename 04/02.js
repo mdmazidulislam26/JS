c28232dc63e18e6dd63b761e51668895cdaefd69
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-06 17:38:40 +0600

    for of

--- 04/02.js
+++ 04/02.js
@@ -13,3 +13,36 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
 
+const arr = [ 1, 2, 3, 4, 5];
+
+for (const element of arr) {
+    console.log(element);
+    
+}
+
+const greeting = "Hello"
+
+for (const greet of greeting) {
+    console.log(greet);
+    
+}
+
+// Maps
+
+const map = new Map();
+
+map.set(1, "Robin");
+map.set(2, "Rohin");
+map.set(3, "Ramim");
+
+console.log(map);
+
+for (const value of map) {
+    console.log(value);
+}
+
+for (const [key, value] of map) {
+    console.log(value);
+}
+
+
