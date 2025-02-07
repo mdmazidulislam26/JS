6e284ebe3b1093d5c57bc4e64df340e024b306e1
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-07 12:29:06 +0600

    if you want to learn promise.just try to understand this code and play with this.

--- 05/01.js
+++ 05/01.js
@@ -6,13 +6,22 @@ const promise001 = new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log(`Async task is complete!!`); //! execute second
         resolve()//! without 'resolve()' line 12 will not execute
-    },2000)//! 2000ms = 2 second
+    },1000)//! 2000ms = 2 second
 })
 
 promise001.then(function(){
     console.log(`Promise consumed!`);//! execute third
 });
 
-console.log(`Hello`); //! execute first
+console.log(`Hello, I am out side the promise`); //! execute first
 
-//? 
+//?
+
+new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log(`Hello`);
+        resolve()
+    },2000)
+}).then(() => {
+    console.log(`Hi`); 
+});
