2000578ac2981c80979bf3fcbe7040fba17f0e2e
Md Mazidul Islam <mazid9326@gmail.com>
2025-03-11 12:13:02 +0600

    basic setup and express server configer done

---
+++ 07/app.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
